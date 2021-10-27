9c8e607f0b73b1c436f4cb89ae8303dc0e47bf37
duoiuoicai <phamvietminhduc@gmail.com>
2021-10-27 16:50:14 +0700

    navbar fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({ totalItems }) => {
         <div>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography component={Link} to="/" variant="h6" className={classes.appBar} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Duck Retail" height="25px" className={classes.image} />
                         Retail Website
                     </Typography>
